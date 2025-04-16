64424f9a9e82d124f3eb5614301afc02757c5649
Felipe <felipealbertosantos@outlook.com>
2025-04-16 16:15:36 -0300

    dev

--- teste/ContentView.swift
+++ teste/ContentView.swift
@@ -7,7 +7,7 @@ struct Gadget: Identifiable {
     let icon: String
     let color: Color
 }
-
+    
 struct GadgetCard: View {
     let gadget: Gadget
     
@@ -52,6 +52,66 @@ struct GadgetCarousel: View {
     }
 }
 
+// explorar
+// troca e doacao
+
+struct TabBar: View {
+    @State var selectedTab = 0
+    var body: some View{
+        
+        TabView{
+            Text("P1")
+                .tabItem{
+                    Image(systemName: "house")
+                        .font(.system(size:25))
+                    Text("Home")
+                }
+                .tag(0)
+            
+            Text("P2")
+                .tabItem{
+                    Image(systemName: "globe")
+                        .font(.system(size:25))
+                    Text("Explorar")
+                }
+                .tag(1)
+            
+            Text("P3")
+                .tabItem{
+                    Image(systemName: "heart.fill")
+                        .font(.system(size:25))
+                    Text("Doação")
+                }
+                .tag(2)
+            
+            Text("P4")
+                .tabItem{
+                    Image(systemName: "person.circle")
+                        .font(.system(size:25))
+                    Text("Perfil")
+                }
+                .tag(3)
+        }
+        .foregroundColor(.black)
+    }
+}
+
+struct AltZstack: View{
+    var body: some View{
+        ZStack{
+            VStack{
+                RoundedRectangle(cornerRadius: 15)
+                    .foregroundColor(.orange)
+                    .frame(width: 343, height: 140)
+                    .cornerRadius(20)
+                    .shadow(color: .black.opacity(0.2), radius: 10, x: 5, y: 0)
+
+            }
+        }
+    }
+}
+
+
 struct ContentView: View {
     let sampleGadgets = [
         Gadget(title: "Leitura", description: "Acompanhe sua leitura e progresso atual", icon: "books.vertical.fill", color: .blue),
@@ -63,10 +123,8 @@ struct ContentView: View {
         VStack {
             HStack {
                 Text("Resumo")
-                    .font(.system(size: 24, weight: .semibold))
+                    .font(.system(size: 30, weight: .semibold))
                 Spacer()
-                Image(systemName: "person.circle")
-                    .font(.system(size:25))
             }
             .padding()
             .padding(.horizontal)
@@ -84,7 +142,25 @@ struct ContentView: View {
             }
             .frame(height: 220)        // Altura fixa para o ScrollView
             
+                VStack {
+                    HStack {
+                        Text("Grupos")
+                            .font(.system(size: 25, weight: .semibold))
+                        Spacer()
+                        Text("Veja todos")
+                            .font(.system(size: 12, weight: .light))
+                            .foregroundColor(.blue)
+                            .padding()
+                    }
+                }
+                .padding()
+                .padding(.horizontal)
+
+            FakeListStructure(book: ["Shreck", "TVD", "Faz o urro"])
+            //AltZstack()
             Spacer()
+            TabBar()
+            
         }
     }
 }
--- teste/testeApp.swift
+++ teste/testeApp.swift
@@ -7,11 +7,54 @@
 
 import SwiftUI
 
+struct TitleStructure: View {
+    let title: String  // No default value, must be provided when creating the view
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(title)
+                    .font(.system(size: 25, weight: .semibold))
+                Spacer()
+            }
+        }
+        .padding()
+        .padding(.horizontal)
+    }
+}
+
+// chamada: TitleStructure(title: "tituloAqui")
+
+struct FakeListStructure: View{
+    let book: [String]
+    
+    var body: some View{
+        //optional
+        //AltZstack()
+        ForEach(book, id: \.self){ books in
+            VStack(spacing: 2){
+                HStack{
+                    Text(books)
+                        .font(.system(size:16, weight: .light))
+                        .padding(.horizontal,4)
+                        .padding(.top, -25)
+                    Spacer()
+                }
+                Divider()
+            }
+            .padding()
+            .padding(.horizontal)
+        }
+    }
+}
+
+// chamada: FakeListStructure(books:["livro1","livro2","livro3])
+
 @main
 struct testeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+                TabBar()
+            }
         }
     }
-}
